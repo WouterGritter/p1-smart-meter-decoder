d87fc6f65facdc6594bfdb5a09978712308aa151
Wouter Gritter <woutergritter@gmail.com>
2021-07-08 17:31:25 +0200

    Switch to pprint

--- examples/example_dummy_file.py
+++ examples/example_dummy_file.py
@@ -1,14 +1,5 @@
 from p1_decode import p1_decode
-
-
-def print_dict(dictionary, spaces=0):
-    for key in dictionary:
-        value = dictionary[key]
-        if type(value) is dict:
-            print(spaces*' ' + f'{key} =')
-            print_dict(value, spaces + 2)
-        else:
-            print(spaces*' ' + f'{key} = {value}')
+import pprint
 
 
 f = open('dummy_reading.txt', 'r')
@@ -16,4 +7,5 @@ dummyReading = f.read()
 
 decoded_reading = p1_decode(dummyReading)
 
-print_dict(decoded_reading)
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(decoded_reading)
